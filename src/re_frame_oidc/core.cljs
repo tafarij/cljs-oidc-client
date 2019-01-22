2e570df44cbcef07fa4e4f4b62c01c9ab50f181e
Tafari Johnson <tafari.johnson@gmail.com>
2019-01-21 19:28:50 -0500

    event handlers and subscriptions

--- src/re_frame_oidc/core.cljs
+++ src/re_frame_oidc/core.cljs
@@ -10,41 +10,76 @@
   [config]
   (UserManager. (clj->js config)))
 
-(defn reg-user-manager
-  ([user-manager]
-   (reg-user-manager default-user-manager-name user-manager))
-  ([user-manager name]
-   (doto user-manager.events
-     (.addUserLoaded #(rf/dispatch [::internals/on-user-loaded name %]))
-     (.addUserUnloaded #(rf/dispatch [::internals/on-user-unloaded name]))
-     (.addAccessTokenExpired #(rf/dispatch [::internals/on-access-token-expired name]))
-     (.addUserSignedOut #(rf/dispatch [::internals/on-user-signed-out name])))))
+(defn- reg-user-manager
+  [user-manager name event-handlers]
+  (let [{:keys [user-loaded
+                user-unloaded
+                access-token-expiring
+                access-token-expired
+                user-signed-out
+                silent-renew-error]} event-handlers]
+    (doto user-manager.events
+      (.addUserLoaded
+       #(do (rf/dispatch [::internals/on-user-loaded name %])
+            (when user-loaded (user-loaded %))))
+      (.addUserUnloaded
+       #(do (rf/dispatch [::internals/on-user-unloaded name])
+            (when user-unloaded (user-unloaded))))
+      (.addAccessTokenExpired
+       #(do (rf/dispatch [::internals/on-access-token-expired name])
+            (when access-token-expired (access-token-expired))))
+      (.addUserSignedOut
+       #(do (rf/dispatch [::internals/on-user-signed-out name])
+            (when user-signed-out (user-signed-out)))))
+    (when access-token-expiring
+      (.addAccessTokenExpiring user-manager.events #(access-token-expiring)))
+    (when silent-renew-error
+      (.addSilentRenewError user-manager.events #(silent-renew-error %)))))
 
 (rf/reg-event-fx
  ::reg-user-manager
- (fn [_ [_ user-manager name]]
+ (fn [_ [_ user-manager name event-handlers]]
    (let [inst-name (or name default-user-manager-name)]
-     (reg-user-manager user-manager inst-name)
+     (reg-user-manager user-manager inst-name (or event-handlers {}))
      {:dispatch [::internals/init inst-name]})))
 
+;; ============================================================================
+;; COMPONENTS
+;; ============================================================================
+
+(defn- callback-mounted
+  [{:keys [user-manager instance-name on-success-fn on-error-fn]}]
+  (let [inst-name (or instance-name default-user-manager-name)]
+    (rf/dispatch [::internals/on-user-loading inst-name])
+    (-> (.signinRedirectCallback user-manager)
+        (.then #(do
+                  (rf/dispatch [::internals/on-user-loaded inst-name %])
+                  (when on-success-fn (on-success-fn %))))
+        (.catch #(do
+                   (rf/dispatch [::internals/on-user-loading-error inst-name])
+                   (when on-error-fn (on-error-fn %)))))))
+
 (defn callback-comp
-  [options & children]
-  (let [{:keys [user-manager instance-name on-success-fn on-error-fn]} options
-        inst-name (or instance-name default-user-manager-name)]
-    (reagent/create-class
-     {:display-name ::callback-comp
+  [props & children]
+  (reagent/create-class
+   {:display-name        :callback-comp
+    :component-did-mount #(callback-mounted props)
+    :reagent-render
+    (fn []
+      (into [:div] (reagent/children (reagent/current-component))))}))
 
-      :component-did-mount
-      (fn []
-        (-> (.signinRedirectCallback user-manager)
-            (.then #(do
-                      (rf/dispatch [::internals/on-user-loaded inst-name %])
-                      (when on-success-fn (on-success-fn %))))
-            (.catch #(do
-                       (rf/dispatch [::internals/on-user-loading-error])
-                       (when on-error-fn (on-error-fn %))))))
+;; ============================================================================
+;; SUBSCRIPTIONS
+;; ============================================================================
 
-      :reagent-render
-      (fn []
-        (into [:div] (reagent/children (reagent/current-component))))})))
+(rf/reg-sub
+ ::instance
+ (fn [db [_ inst-name]]
+   (get-in db [:rf-oidc (or inst-name default-user-manager-name)])))
 
+(rf/reg-sub
+ ::user
+ (fn [_ [_ inst-name]]
+   (rf/subscribe [::instance inst-name]))
+ (fn [inst _]
+   (:user inst)))
